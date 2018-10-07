eb8570dd98e44604e39c559fdb2e27767c3c5c71
Nagendra <nagendra_babu@elementblue.in>
2018-10-07 23:14:53 +0400

    new item list design

--- lib/recents_page.dart
+++ lib/recents_page.dart
@@ -44,32 +44,52 @@ class _RecentPageState extends State<RecentsPage> {
     return employees;
   }
 
-  final makeListTile = ListTile(
-      contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-      leading: Container(
-        padding: EdgeInsets.only(right: 12.0),
-        decoration: new BoxDecoration(
-            border: new Border(
-                right: new BorderSide(width: 1.0, color: Colors.white24))),
-        child: Icon(Icons.autorenew, color: Colors.white),
-      ),
-      title: Text(
-        "Introduction to Driving",
-        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-      ),
-      // subtitle: Text("Intermediate", style: TextStyle(color: Colors.white)),
-
-      subtitle: Row(
-        children: <Widget>[
-          Icon(Icons.linear_scale, color: Colors.yellowAccent),
-          Text(" Intermediate", style: TextStyle(color: Colors.white))
-        ],
-      ),
-      trailing:
-          Icon(Icons.keyboard_arrow_right, color: Colors.white, size: 30.0));
-
   @override
   Widget build(BuildContext context) {
+    final makeListTile = ListTile(
+        contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        leading: Container(
+          padding: EdgeInsets.only(right: 12.0),
+          decoration: new BoxDecoration(
+              border: new Border(
+                  right: new BorderSide(width: 1.0, color: Colors.white24))),
+          child: Icon(Icons.autorenew, color: Colors.white),
+        ),
+        title: Text(
+          "Introduction to Driving",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        // subtitle: Text("Intermediate", style: TextStyle(color: Colors.white)),
+
+        subtitle: Row(
+          children: <Widget>[
+            Icon(Icons.linear_scale, color: Colors.yellowAccent),
+            Text(" Intermediate", style: TextStyle(color: Colors.white))
+          ],
+        ),
+        trailing:
+            Icon(Icons.keyboard_arrow_right, color: Colors.white, size: 30.0));
+
+    final makeCard = Card(
+      elevation: 8.0,
+      margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+      child: Container(
+        decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
+        child: makeListTile,
+      ),
+    );
+
+    final makeBody = Container(
+      child: ListView.builder(
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
+        itemCount: 10,
+        itemBuilder: (BuildContext context, int index) {
+          return makeCard;
+        },
+      ),
+    );
+
     return new Scaffold(
         floatingActionButton: FloatingActionButton(
             child: Icon(Icons.add),
@@ -94,20 +114,21 @@ class _RecentPageState extends State<RecentsPage> {
                         return Center(child: CircularProgressIndicator());
                       default:
                         if (snapshot.hasData) {
-                          return new ListView.builder(
-                              itemCount: snapshot.data.length,
-                              itemBuilder: (context, index) {
-                                return new Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: <Widget>[
-                                      new Text(snapshot.data[index].name,
-                                          style: _textStyle(18.0)),
-                                      new Text(snapshot.data[index].mobileNo,
-                                          style: _textStyle(14.0)),
-                                      new Divider()
-                                    ]);
-                              });
+                          return makeBody;
+                          // return new ListView.builder(
+                          //     itemCount: snapshot.data.length,
+                          //     itemBuilder: (context, index) {
+                          //       return new Column(
+                          //           crossAxisAlignment:
+                          //               CrossAxisAlignment.start,
+                          //           children: <Widget>[
+                          //             new Text(snapshot.data[index].name,
+                          //                 style: _textStyle(18.0)),
+                          //             new Text(snapshot.data[index].mobileNo,
+                          //                 style: _textStyle(14.0)),
+                          //             new Divider()
+                          //           ]);
+                          //     });
                         } else if (snapshot.error ||
                             snapshot.data.length == 0) {
                           return new Text("${snapshot.error}");
